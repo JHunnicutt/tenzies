dffa45eeb102be40c0d0a004f765196e6d094a2f
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2022-08-19 06:32:36 -0400

    add basic dice structure without logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Die from './Die'
+
 
 const App = () => {
-   
+   return (
+    <main>
+      <div className="dice">
+         <Die value="1" />
+         <Die value="1" />
+         <Die value="1" />
+         <Die value="1" />
+         <Die value="1" />
+         <Die value="1" />
+         <Die value="1" />
+         <Die value="1" />
+         <Die value="1" />
+         <Die value="1" />
+      </div>
+    </main>
+   )
 }
 
 export default App
